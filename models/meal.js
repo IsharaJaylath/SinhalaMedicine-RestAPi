a37a03a38f38afb0d8498f5a7f489010c75cb1b1
ThariduIT <58337616+ThariduIT@users.noreply.github.com>
2020-06-07 00:56:46 +0530

    Update meal.js

--- models/meal.js
+++ models/meal.js
@@ -1,1 +1,23 @@
+const mongoose = require('mongoose');
+
+const mealSchema = new mongoose.Schema({
+    title: {
+       type: String,
+       required: true
+    },
+    ingrediants: {
+        type: Array,
+        required: true
+
+    },
+    description:{
+        type: String,
+        required: true
+    }
+})
+
+
+
+
+module.exports = mongoose.model('meal',mealSchema);
 
