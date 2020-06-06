d83293c9e4c422fa83fb9aebb8f049726c897868
ThariduIT <58337616+ThariduIT@users.noreply.github.com>
2020-06-07 00:59:44 +0530

    Add files via upload

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+ 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+
+    },
+    password:{
+        type: String,
+        required: true
+    }
+})
+
+
+module.exports = mongoose.model('user',userSchema);
